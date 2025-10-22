2f62947659af1c05370af5098c51f45cde548d59
jack-mcnulty <jack.mcnulty@stu.ocsb.ca>
2025-10-22 13:26:55 -0400

    turns on a servo motor

--- main.ts
+++ main.ts
@@ -1,8 +1,30 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack McNulty
+ * Created on: Oct 2025
+ * This program tunrs a servo motor
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup 
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// turning to 0
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    basic.showString("0")
+    basic.showIcon(IconNames.SmallSquare)
+    robotbit.Servo(servoNumber1, 0)
+    basic.showIcon(IconNames.Happy)
+})
+ // turning to 180
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("180")
+    basic.showIcon(IconNames.SmallSquare)
+    robotbit.Servo(servoNumber1, 180)
+    basic.showIcon(IconNames.Happy)
+})
